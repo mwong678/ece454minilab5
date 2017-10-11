f7590a96d535da84f1f8d2a067e08f12e8633d09
Matthew  Wong <matthewwong@Matthews-MacBook-Pro.local>
2017-10-11 13:16:56 -0500

    Initial commit

---
+++ app/src/main/java/edu/wisc/ece/ece454minilab5/ListViewLayoutActivity.java
@@ -0,0 +1,55 @@
+/*
+Matthew Wong
+ECE 454 Minilab 5
+ */
+
+
+package edu.wisc.ece.ece454minilab5;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ListViewLayoutActivity extends ListActivity {
+
+    private ArrayAdapter<String> mAdapter;
+    private ArrayList<String> FOLKS = new ArrayList<String>(Arrays.asList("studentA", "studentB", "studentC", "studentD"));
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Define a new adapter
+        mAdapter = new ArrayAdapter<String>(this,
+                R.layout.activity_list_view_layout, FOLKS);
+
+        // Assign the adapter to ListView
+        setListAdapter(mAdapter);
+
+        // Define the listener interface
+        OnItemClickListener mListener = new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                String elementText = (String) parent.getItemAtPosition(position); // convert to string
+                FOLKS.remove(position);
+                mAdapter.notifyDataSetChanged();
+                Toast.makeText(getApplicationContext(), elementText +  " is removed from the list!", Toast.LENGTH_SHORT).show();
+
+            }
+        };
+
+        // Get the ListView and wired the listener
+        ListView listView = getListView();
+        listView.setOnItemClickListener(mListener);
+
+    }
+}
